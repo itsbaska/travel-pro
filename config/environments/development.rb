6257a2b0102ed20300a19747876a40d97a04d3b1
Victoria Luc <victoriajeniluc@gmail.com>
2017-11-11 11:37:26 -0600

    resize the photo to 1200

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,10 +55,10 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/"
   config.paperclip_defaults = {
         storage: :s3,
+        bucket: ENV['AWS_BUCKET_NAME'],
         s3_credentials: {
           s3_region: "us-east-2",
           s3_host_name: ENV['S3_HOST_NAME'],
-          bucket: ENV['S3_BUCKET_NAME'],
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
         }
