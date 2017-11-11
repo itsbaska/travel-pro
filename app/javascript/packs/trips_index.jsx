7ade710e52c78e44ab3ac8ad1f69df12a04db90e
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-10 22:50:31 -0600

    showing all trips

--- app/javascript/packs/trips_index.jsx
+++ app/javascript/packs/trips_index.jsx
@@ -6,26 +6,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import HelloWorld from '../components/HelloWorld'
+import Trips from '../components/Trips'
 
 
 
-const Hello = props => (
-  <div>Hello {props.name}!
-    Hello {props.message}!
+const Trip = props => (
+  <div>
+    <Trips data={props.trips}/>
   </div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('hello-react')
-  const data = JSON.parse(node.getAttribute('data'))
+  const node2 = document.getElementById('all-trips')
+  const data2 = JSON.parse(node2.getAttribute('data'))
+  ReactDOM.render(<Trip {...data2}/>, node2)
 
-  ReactDOM.render(<Hello {...data} />, node)
 })
