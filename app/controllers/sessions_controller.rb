ff365a7089212f1cfbb0eb47badc022a7a5a6249
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-11 15:56:28 -0600

    login a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,35 @@
-class SessionsController < ApplicationController
+class SessionsController < Devise::SessionsController
   def new
-    @user = "sessionasdfasdfads"
+    @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.find_by_email(user_params[:email])
+    return invalid_login_attempt unless @user
+
+    if @user.valid_password?(user_params[:password])
+      p "I AM LOGGED IN"
+      sign_in :user, @user
+      redirect_to user_dashboard_path(@user)
+    else
+      p "I AM NOT LOGGED IN"
+      invalid_login_attempt
+    end
+  end
+
+  def destroy
+    sign_out(@user)
+    render json: {success: true}
+  end
+
+  private
+
+  def invalid_login_attempt
+    warden.custom_failure!
+    render json: {error: 'Invalid login attempt'}, status: :unprocessable_entity
+  end
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
