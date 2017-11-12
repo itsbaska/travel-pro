8c8e746fb4682d7ba8b1267405697748ccb8c871
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-11 18:30:46 -0600

    register form posting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < Devise::RegistrationsController
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      p "I am registered"
+      redirect_to user_dashboard_path(@user)
     else
+      p "I am not registered :("
+      p  @user.errors.full_messages
       warden.custom_failure!
       render json: { error: 'signup error' }, status: :unprocessable_entity
     end
@@ -33,6 +39,6 @@ class UsersController < Devise::RegistrationsController
   private
 
   def user_params
-     params.require(:user).permit(:email, :password, :password_confirmation)
+     params.permit(:first_name, :last_name, :phone_number, :email, :password, :password_confirmation)
   end
 end
