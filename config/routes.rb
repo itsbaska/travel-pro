50c3219977d7af9032fc83eb65b223030542e9c2
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-11 21:14:41 -0600

    remove devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users', sessions: 'sessions'}
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :trips
 
-  # get "/register", to: "users#new"
-  # post "/users", to: "users#create"
+  get '/users/sign_up', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
 
-
-  # get "/login", to: "sessions#new"
-  # post "/login", to: "sessions#create"
-  # delete "/logout", to: "sessions#destroy"
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get "/users/:id/dashboards", to: "dashboards#show", as: 'user_dashboard'
 
   root to: "trips#index"
