5bd766b505cfdce6e1d561280abf5d0cc0078365
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-12 15:28:33 -0600

    inbound creating

--- app/javascript/components/NewInbound.js
+++ app/javascript/components/NewInbound.js
@@ -1,28 +1,87 @@
 import React from "react"
 import PropTypes from "prop-types"
+import $ from 'jquery'
+
 
 class NewInbound extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      airline: '',
+      airport: '',
+      arrival: '',
+      depature: '',
+      trip: props.data,
+      errors: props.errors || ''
+    }
+    console.log(this.state.errors)
+    this.handleNewInboundChange = this.handleNewInboundChange.bind(this)
+    this.handleNewInboundSubmit = this.handleNewInboundSubmit.bind(this)
+  }
+
+  handleNewInboundChange(input) {
+    this.setState({value: input})
+  }
+
+  handleNewInboundSubmit(){
+    var form = new FormData(document.getElementById('outbound-new-form'))
+    fetch("http://localhost:3000/trips/"+ this.state.trip.id +"/outbounds/new", {
+      method: "POST",
+      headers: {'X-CSRF-Token': token},
+      body: form
+    }).then((response) => response.json())
+  }
+
   render () {
-    return (
-      <form className="inbound-new-form">
-        <h2>Add Booking Info</h2>
-        <h4>Inbound</h4>
+    const token = $('meta[name="csrf-token"]').attr('content');
 
-        <label htmlFor="airport">Airport:</label>
-        <input type="text" name="airport"/>
+    if (this.state.errors.length > 0 ) {
+      return (
+        <form className="inbound-new-form" method="post" action={"/trips/" + this.state.trip.id + "/inbounds"} onSubmit={this.handleNewInboundSubmit}>
+        <input type="hidden" name="authenticity_token" value={token} readOnly={true} />
 
-        <label htmlFor="airline">Airline:</label>
-        <input type="text" name="airline"/>
+          <h2>Add Booking Info</h2>
+          <h4>Inbound</h4>
 
-        <label htmlFor="arrival">Arrival:</label>
-        <input type="datetime-local" name="arrival"/>
+          <label htmlFor="airport">Airport:</label>
+          <input type="text" name="airport" onChange={this.handleNewInboundChange} />
 
-        <label htmlFor="departure">Departure:</label>
-        <input type="datetime-local" name="departure"/>
+          <label htmlFor="airline">Airline:</label>
+          <input type="text" name="airline" onChange={this.handleNewInboundChange} />
 
-        <a href="/">Save</a> 
-      </form>
-    );
+          <label htmlFor="arrival">Arrival:</label>
+          <input type="datetime-local" name="arrival" onChange={this.handleNewInboundChange} />
+
+          <label htmlFor="departure">Departure:</label>
+          <input type="datetime-local" name="departure" onChange={this.handleNewInboundChange} />
+
+           <input type="submit" value="Save" />
+        </form>
+      );
+    } else {
+      return (
+        <form className="inbound-new-form" method="post" action={"/trips/" + this.state.trip.id + "/inbounds"} onSubmit={this.handleNewInboundSubmit}>
+          <input type="hidden" name="authenticity_token" value={token} readOnly={true} />
+
+          <h2>Add Booking Info</h2>
+          <h4>Inbound</h4>
+
+          <label htmlFor="airport">Airport:</label>
+          <input type="text" name="airport" onChange={this.handleNewInboundChange}/>
+
+          <label htmlFor="airline">Airline:</label>
+          <input type="text" name="airline" onChange={this.handleNewInboundChange}/>
+
+          <label htmlFor="arrival">Arrival:</label>
+          <input type="datetime-local" name="arrival" onChange={this.handleNewInboundChange}/>
+
+          <label htmlFor="departure">Departure:</label>
+          <input type="datetime-local" name="departure" onChange={this.handleNewInboundChange}/>
+
+           <input type="submit" value="Save" />
+        </form>
+      )
+    }
   }
 }
 
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,8 @@ class Trip < ApplicationRecord
   has_many :receipts
   belongs_to :organizer, class_name: "User"
   has_many :travellers, through: :travelgroupings, source: :traveller
+  has_many :inbound_flights, through: :travelgroupings, source: :inbounds
+  has_many :outbound_flights, through: :travelgroupings, source: :outbounds
 
   validates :name, :location, :budget, presence: true
   validates_numericality_of :budget, :greater_than => 0
