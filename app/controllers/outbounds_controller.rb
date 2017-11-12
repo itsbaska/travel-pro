c112bd51e267d7d721d912d5999187496071d48e
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-12 13:36:58 -0600

    able to create a trip by organizer

--- app/controllers/outbounds_controller.rb
+++ app/controllers/outbounds_controller.rb
@@ -1,6 +1,26 @@
 class OutboundsController < ApplicationController
-  def new 
-  	@trip = Trip.find(params[:trip_id])
-  	@outbound = Outbound.new 
+  def new
+    return authenticate!
+    @trip = Trip.find(params[:trip_id])
+    @outbound = Outbound.new
   end
+
+  def create
+    return authenticate!
+    @trip = Trip.find(params[:trip_id])
+    @travelgroup = Travelgrouping.new(traveller: current_user, trip: @trip)
+    @outbound = Outbound.new(flight_params)
+    @outbound.travelgrouping = @travelgroup
+
+    if @outbound.save
+      redirect_to @trip
+    else
+      render component: 'NewOutbound', props: {errors: @outbound.errors.full_messages}
+    end
+  end
+
+  private
+    def flight_params
+      params.permit(:airline, :arrival, :departure, :airport)
+    end
 end
