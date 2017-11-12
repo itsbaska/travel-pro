04ef41b1731e888aa377213dd0f2e7f0c63d05d0
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-11 18:45:10 -0600

    error handling for registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,37 +6,17 @@ class UsersController < Devise::RegistrationsController
   def create
     @user = User.new(user_params)
     if @user.save
-      p "I am registered"
       redirect_to user_dashboard_path(@user)
     else
-      p "I am not registered :("
-      p  @user.errors.full_messages
-      warden.custom_failure!
-      render json: { error: 'signup error' }, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @user = User.find_by_email(user_params[:email])
-
-    if @user.update_attributes(user_params)
-      render json: @user
-    else
-      warden.custom_failure!
-      render :json=> @user.errors, :status=>422
-    end
- end
-
-  def destroy
-    @user = User.find_by_email(user_params[:email])
-    if @user.destroy
-      render :json=> { success: 'user was successfully deleted' }, :status=>201
-    else
-      render :json=> { error: 'user could not be deleted' }, :status=>422
+      invalid_register(@user)
     end
   end
 
   private
+
+  def invalid_register(user)
+     render component: 'Register', props: { errors: user.errors.full_messages }
+  end
 
   def user_params
      params.permit(:first_name, :last_name, :phone_number, :email, :password, :password_confirmation)
--- app/javascript/components/Register.js
+++ app/javascript/components/Register.js
@@ -4,13 +4,14 @@ import PropTypes from "prop-types"
 class Register extends React.Component {
   constructor(props) {
     super(props)
+    console.log(props)
       this.state = {
         first_name: '',
         last_name: '',
         email: '',
         phone_number: '',
         password: '',
-        error: props.errors || ""
+        errors: props.errors || ""
       }
 
     this.handleRegisterChange = this.handleRegisterChange.bind(this)
@@ -33,28 +34,56 @@ class Register extends React.Component {
   }
 
   render () {
-    return (
+    if (this.state.errors.length > 0) {
+      return (
+      <div>
+      <h1>{this.state.errors}</h1>
       <form className="register-form" method="post" action="/users" onSubmit={this.handleRegisterForm}>
-        <h2>Register</h2>
+          <h2>Register</h2>
 
-        <label htmlFor="first_name">First Name</label>
-        <input type="text" name="first_name" onChange={this.handleRegisterChange}/>
+          <label htmlFor="first_name">First Name</label>
+          <input type="text" name="first_name" onChange={this.handleRegisterChange}/>
 
-        <label htmlFor="last_name">Last Name</label>
-        <input type="text" name="last_name" onChange={this.handleRegisterChange}/>
+          <label htmlFor="last_name">Last Name</label>
+          <input type="text" name="last_name" onChange={this.handleRegisterChange}/>
 
-        <label htmlFor="phone_number">Phone Number</label>
-        <input type="text" name="phone_number" onChange={this.handleRegisterChange}/>
+          <label htmlFor="phone_number">Phone Number</label>
+          <input type="text" name="phone_number" onChange={this.handleRegisterChange}/>
 
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" onChange={this.handleRegisterChange}/>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" onChange={this.handleRegisterChange}/>
 
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" onChange={this.handleRegisterChange}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onChange={this.handleRegisterChange}/>
 
-        <button type="submit">Register</button>
+          <button type="submit">Register</button>
       </form>
-    );
+      </div>
+      );
+    } else {
+      return (
+        <form className="register-form" method="post" action="/users" onSubmit={this.handleRegisterForm}>
+          <h2>Register</h2>
+
+          <label htmlFor="first_name">First Name</label>
+          <input type="text" name="first_name" onChange={this.handleRegisterChange}/>
+
+          <label htmlFor="last_name">Last Name</label>
+          <input type="text" name="last_name" onChange={this.handleRegisterChange}/>
+
+          <label htmlFor="phone_number">Phone Number</label>
+          <input type="text" name="phone_number" onChange={this.handleRegisterChange}/>
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" onChange={this.handleRegisterChange}/>
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onChange={this.handleRegisterChange}/>
+
+          <button type="submit">Register</button>
+        </form>
+      );
+    }
   }
 }
 
