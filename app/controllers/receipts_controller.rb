2f841cbc8c8b1649950b6fae64f1b0c18fb704b9
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-12 17:28:24 -0600

    Show all receipts of the trip

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,13 +1,19 @@
 class ReceiptsController < ApplicationController
+  def index
+    @trip = Trip.find(params[:trip_id])
+    @receipts = @trip.receipts
+  end
+
   def new
     @trip = Trip.find(params[:trip_id])
   end
 
   def create
     @trip = Trip.find(params[:trip_id])
+    p receipt_params
     @receipt = Receipt.new(receipt_params)
     if @receipt.save
-      redirect_to receipt_path(@receipt)
+      redirect_to trip_receipt_path(@receipt)
     else
       render action: "new"
     end
@@ -19,6 +25,6 @@ class ReceiptsController < ApplicationController
 
   private
   def receipt_params
-    params.require(:receipt).permit(:total, :store, :photo).merge(purchaser: current_user, trip: @trip)
+    params.permit(:total, :store, :photo).merge(purchaser: current_user, trip: @trip)
   end
 end
