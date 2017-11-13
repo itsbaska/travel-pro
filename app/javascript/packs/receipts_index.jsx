95e88c891624abe1494cc73594d7aa8e159260d0
Melissa Kanhirun <mk7run@gmail.com>
2017-11-12 18:13:47 -0600

    Update receipts_index.jsx

--- app/javascript/packs/receipts_index.jsx
+++ app/javascript/packs/receipts_index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import HelloWorld from '../components/HelloWorld'
 import Receipts from '../components/Receipts'
 
 
