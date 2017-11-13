f9cf7bfd37805ebdf72161dbd4e848b932f72834
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-13 15:10:39 -0600

    fake chart

---
+++ app/javascript/components/Chart.js
@@ -0,0 +1,43 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {Pie} from 'react-chartjs-2'
+
+class Chart extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      chartData: props.chartData
+    }
+  }
+
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: 'right',
+    location: 'City'
+  }
+
+
+  render () {
+    return (
+      <div className="chart">
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title:{
+              display: this.props.displayTitle,
+              text: "Largest City" + this.props.location,
+              fontSize: 25
+            },
+            legend:{
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+          />
+      </div>
+    )
+  }
+}
+
+export default Chart
