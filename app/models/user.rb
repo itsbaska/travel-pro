65fd256b5de831a695a428b72c09887844d605f4
Melissa Kanhirun <mk7run@gmail.com>
2017-11-13 15:59:46 -0600

    Add more attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-
+  before_save :full_name
   has_many :travelgroupings, foreign_key: :traveller_id
   has_many :trips, foreign_key: :organizer_id
   has_many :receipts, foreign_key: :purchaser_id
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, :phone_number, presence: true
   validates :email, uniqueness: true,  :format => { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   validates :phone_number, :format => { with: /\d{3}-\d{3}-\d{4}/ }
+
+  def full_name
+    self.full_name = self.first_name + " " + self.last_name
+  end
 end
