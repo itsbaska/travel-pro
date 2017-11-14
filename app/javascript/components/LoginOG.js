5b8adce1c5e0b4d4d9bda26bc597e6ed790a3611
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-14 14:57:12 -0600

    some mailer updat

---
+++ app/javascript/components/LoginOG.js
@@ -0,0 +1,73 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        email: '',
+        password: '',
+        error: props.errors || ""
+      }
+
+    this.handleLoginChange = this.handleLoginChange.bind(this)
+    this.handleLoginForm = this.handleLoginForm.bind(this)
+  }
+
+  handleLoginChange(input) {
+    this.setState({value: input})
+  }
+
+  handleLoginForm() {
+
+    var form = new FormData(document.getElementById('login-form'))
+    fetch('http://localhost:3000/login', {
+      method: "POST",
+      headers: {'X-CSRF-Token': token
+      },
+      body: form
+    }).then((response) => response.json())
+  }
+
+  render () {
+    if (this.state.error.length > 0 ) {
+      return (
+        <div>
+
+        <form className="login-form" onSubmit={this.handleLoginForm} method="post" action="/sessions">
+          <h2>Login</h2>
+
+          <ul className="error">
+            <li>{this.state.error}</li>
+          </ul>
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" onChange={this.handleLoginChange} />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onChange={this.handleLoginChange} />
+
+          <button type="submit">Login</button>
+        </form>
+        </div>
+        )
+      } else {
+        return (
+        <form className="login-form" onSubmit={this.handleLoginForm} method="post" action="/sessions">
+          <h2>Login</h2>
+
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" onChange={this.handleLoginChange} />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onChange={this.handleLoginChange} />
+
+          <button type="submit">Login</button>
+        </form>
+      )
+    }
+  }
+}
+
+export default Login
