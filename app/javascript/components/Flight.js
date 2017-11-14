b7cbc45e8cf2e28a4a58f2873e5847e57a153f4e
Melissa Kanhirun <mk7run@gmail.com>
2017-11-13 18:47:39 -0600

    Add more details to views

--- app/javascript/components/Flight.js
+++ app/javascript/components/Flight.js
@@ -5,7 +5,8 @@ class Flight extends React.Component {
   render () {
     return (
       <li>
-        <h4 className="airline-name">{this.props.airline}</h4>
+        <h4>{this.props.traveller_name}</h4>
+        <p className="airline-name">{this.props.airline}</p>
         <p className="airline-arrival">{this.props.arrival}</p>
         <p className="airline-departure">{this.props.departure}</p>
         <p className="airport-name">{this.props.airport}</p>
