2dee3e73d50f0aaa8fc5fa237eedbd61ca43f5d3
Melissa Kanhirun <mk7run@gmail.com>
2017-11-13 18:47:39 -0600

    Add more details to views

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,5 @@ class DashboardsController < ApplicationController
   def show
     authenticate!
     @trips = Trip.where(organizer_id: params[:id])
-    @organizer = User.find(params[:id])
   end
 end
--- app/javascript/components/Receipt.js
+++ app/javascript/components/Receipt.js
@@ -6,6 +6,7 @@ class Receipt extends React.Component {
     return (
       <li>
         <p className="store-name">Name: {this.props.store}</p>
+        <p>Purchased by: {this.props.purchaser_name}</p>
         <p className="receipt-total">Total: {this.props.total}</p>
         <img src={"https:"+this.props.photo_url}/>
       </li>
