1d0df242acd12fac68a2fba151c14de417c9b2b4
Victoria Luc <victoriajeniluc@gmail.com>
2017-11-15 17:49:02 -0600

    add one more rspec test for receipts

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -11,6 +11,15 @@ describe Receipt do
 			expect(receipt).to be_valid 
 		end 
 
+		it "validates the total" do 
+			purchaser = User.create(first_name: "Victoria", last_name: "Luc", phone_number: "847-312-4925", email: "victoriajeniluc@gmail.com", password: "password")
+
+  		trip = Trip.create(name: "Reunion", location: "DBC", budget: 1.30, start_date: Date.strptime("1/17/2018", "%m/%d/%Y"), end_date: Date.strptime("1/22/2018", "%m/%d/%Y"), organizer: purchaser)
+
+			receipt = Receipt.create(store: "", total: 100, photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj9pcOF38HXAhVEyFQKHUjTAc8QjRwIBw&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FReceipt&psig=AOvVaw1wWRl3Q6EFG_98G0W7lpW2&ust=1510875247017688", trip: trip, purchaser: purchaser)
+			expect(receipt.total).to eq(100) 
+		end 
+
 		it "is not valid without the store" do 
 			purchaser = User.create(first_name: "Victoria", last_name: "Luc", phone_number: "847-312-4925", email: "victoriajeniluc@gmail.com", password: "password")
 
