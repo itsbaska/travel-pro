46dd6e98f162ec068d8ec0e1ee8485b870d473b5
Baska Batbold & Melissa Kanhirun & Victoria Luc <pair+itsbaska.mk7run.victoriajeniluc@devbootcamp.com>
2017-11-16 16:23:46 -0600

    attempt at adding a background to the welcom/home page

--- app/javascript/packs/trips_index.jsx
+++ app/javascript/packs/trips_index.jsx
@@ -6,7 +6,10 @@ import PropTypes from 'prop-types'
 
 const Trip = props => (
   <div className="main-content home-page">
-  <img className="home-image" src="../../``````assets/images/home_photo.jpeg"/>
+    <div className="button-container">
+      <a href="/login"><button className="login-button">Login</button></a>
+      <a href="/sign-up"><button className="register-button">Register</button></a>
+    </div>
   </div>
 )
 
@@ -14,5 +17,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const node2 = document.getElementById('all-trips')
   const data2 = JSON.parse(node2.getAttribute('data'))
   ReactDOM.render(<Trip {...data2}/>, node2)
-
 })
