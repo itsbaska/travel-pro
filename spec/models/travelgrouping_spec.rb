4bac7df4439d9fc94ade3e0da6b172a43c83800a
Baska Batbold & Melissa Kanhirun <pair+itsbaska.mk7run@devbootcamp.com>
2017-11-30 13:24:51 -0600

    Change travelgroupings tests to factory bot

--- spec/models/travelgrouping_spec.rb
+++ spec/models/travelgrouping_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
-describe Travelgrouping do
-  let(:travelgrouping) { Travelgrouping.new }
+describe FactoryBot do
+  let(:factory)  { FactoryBot::Factory.new(:travelgrouping) }
 
-  describe "assocations" do 
-  	it { Travelgrouping.reflect_on_association(:trip).macro.should eq(:belongs_to)}
-    it { Travelgrouping.reflect_on_association(:outbounds).macro.should eq(:has_many)}
-    it { Travelgrouping.reflect_on_association(:inbounds).macro.should eq(:has_many)}
-    it { Travelgrouping.reflect_on_association(:traveller).macro.should eq(:belongs_to)}
-  end 
-end 
+  it "finds a registered factory" do
+    FactoryBot.register_factory(factory)
+    expect(FactoryBot.factory_by_name(factory.name)).to eq factory
+  end
+end
+
+RSpec.describe Travelgrouping, type: :model do
+  context "associations" do
+    it { should have_many(:inbounds) }
+    it { should have_many(:outbounds) }
+    it { should belong_to(:traveller) }
+    it { should belong_to(:trip) }
+  end
+end
+
+
+# describe Travelgrouping do
+#   let(:travelgrouping) { Travelgrouping.new }
+
+#   describe "associations" do
+#   	it { Travelgrouping.reflect_on_association(:trip).macro.should eq(:belongs_to)}
+#     it { Travelgrouping.reflect_on_association(:outbounds).macro.should eq(:has_many)}
+#     it { Travelgrouping.reflect_on_association(:inbounds).macro.should eq(:has_many)}
+#     it { Travelgrouping.reflect_on_association(:traveller).macro.should eq(:belongs_to)}
+#   end
+# end
